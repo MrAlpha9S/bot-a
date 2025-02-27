bdd080016ddf22c60698ec0f9b02c1c6e8c52eab
MrAlpha9S <mr28042005@gmail.com>
2025-02-28 00:56:53 +0700

    [EMBED]

--- commands/utility/show.js
+++ commands/utility/show.js
@@ -1,10 +1,40 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
+const path = require('path');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('show')
-		.setDescription('Show player information.'),
+		// .addStringOption(option =>
+		// 	option.setName('id')
+		// 		.setDescription('Input CardID'))
+	    .setDescription('Show Card information.'),
 	async execute(interaction) {
-		await interaction.reply('Ping!');
+		const id = interaction.options.getString('id');
+
+		const picPath = path.join(__dirname, '../../pic/PR00001.jpg');
+		const iconPath = path.join(__dirname, '../../pic/icon.jpg');
+		const picAttachment = new AttachmentBuilder(picPath);
+		const iconAttachment = new AttachmentBuilder(iconPath);
+
+		const embed = new EmbedBuilder()
+		.setColor(0xFFFFFF)
+		.setTitle('Name')
+		.setDescription('Some description here')
+		.setThumbnail('attachment://icon.jpg')
+		.addFields(
+			{ name: 'Passive:', value: 'Some value here' },
+			{ name: 'PAC ', value: '', inline: true },
+			{ name: 'SHO ', value: '', inline: true },
+			{ name: 'PAS ', value: '', inline: true },
+			{ name: 'DRI ', value: '', inline: true },
+			{ name: 'DEF ', value: '', inline: true },
+			{ name: 'PHY ', value: '', inline: true },
+		)
+		.addFields({ name: 'Skill 1:', value: 'Some value here', inline: true })
+		.addFields({ name: 'Skill 2:', value: 'Some value here', inline: true })
+		.setImage('attachment://PR00001.jpg') // Use the attachment name here
+		.setTimestamp();
+
+		await interaction.reply({ embeds: [embed], files: [picAttachment, iconAttachment] });
 	},
 };
--- connect.js
+++ connect.js
@@ -2,15 +2,15 @@ const mysql = require('mysql2');
 
 const conn = mysql.createConnection({
   host: 'localhost',
-  database: 'BlueLock',
+  database: 'sa',
   user: 'root',
-  password: '123456',
+  password: '12345',
   port: 3307,
 });
 
 // conn.query('SELECT * FROM Cards', function (err, results, fields) {
-//   console.log(results);
-//   console.log(fields); 
+//   console.log(results); // results contains rows returned by server
+//   console.log(fields); // fields contains extra meta data about results, if available
 // });
 
 module.exports = { conn }
